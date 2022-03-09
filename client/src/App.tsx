63d544a0abdbdd2f6bd7183f08995eb7de391ef2
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-03-09 12:44:45 -0600

    changed to typescript project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar.js';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
