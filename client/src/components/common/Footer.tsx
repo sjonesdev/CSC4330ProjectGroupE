20167e60ec2c0349aacca5e9a229ca27599ade13
EfficiencyIsKey <kjoh345@lsu.edu>
2022-03-13 17:45:48 -0500

    just added the home section to footer

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -20,7 +20,7 @@ class Footer  extends React.Component<FooterProps, FooterState> {
               <ul>
                   <li><Link to ="/about">About</Link></li>
                   <li><Link to ="/about#contact-us">Contact Us</Link></li>
-                  <li></li>
+                  <li><Link to = "/home">home</Link></li>
               </ul>  
             </div>
         );
