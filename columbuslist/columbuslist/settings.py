8e24ef31a8c79530ba64cd0fa882f8c0a2e14828
bkab1 <bkabba1@lsu.edu>
2022-04-04 16:14:48 -0500

    Authentication;create;login;logout

--- columbuslist/columbuslist/settings.py
+++ columbuslist/columbuslist/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'knox',
+    'server',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication')
+}
