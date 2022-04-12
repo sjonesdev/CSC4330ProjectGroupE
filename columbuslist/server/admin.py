e70e73d74480041b0f5028e7a39002e71566e20b
mkimdd <94936983+mkimdd@users.noreply.github.com>
2022-04-12 02:14:17 -0500

    reworked and expanded API for frontend intergration

--- columbuslist/server/admin.py
+++ columbuslist/server/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Listing, WishlistListing
+from .models import User, Listing, WishlistListing, Tag
 
 admin.site.register(User)
 admin.site.register(Listing)
 admin.site.register(WishlistListing)
+admin.site.register(Tag)
--- columbuslist/server/serializers.py
+++ columbuslist/server/serializers.py
@@ -1,17 +1,39 @@
+from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
-from .models import User, Listing, WishlistListing
+from .models import User, Listing, WishlistListing, Tag
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=(["username"])
 
-class ListingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Listing
-        fields=("title", "description", "price", "contact", "user")
-
 class WishlistListingSerializer(serializers.ModelSerializer):
     class Meta:
         model=WishlistListing
         fields=("listing", "user")
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Tag
+        fields=(["name"])
+
+class ListingSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+    #title = serializers.CharField(max_length=255)
+    #description = serializers.CharField(max_length=255)
+    #price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    #contact = serializers.CharField(max_length=255)
+
+    class Meta:
+        model=Listing
+        fields=("title", "description", "price", "contact", "user", "tags")
+
+    #    def update(self, instance, validated_data):
+    #        print(validated_data)
+    #        instance.title = validated_data.get('title', instance.title)
+    #        instance.description = validated_data.get('description', instance.description)
+    #        instance.price = validated_data.get('price', instance.price)
+    #        instance.contact = validated_data.get('contact', instance.contact)
+    #        instance.tags = validated_data.get('tags', instance.tags)
+    #        instance.save()
+    #        return instance
