965212494ea501201cfff7d72773f547bc32a1e1
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-04-26 12:26:38 -0500

    added 2 team photos and appropriate styling

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -31,9 +31,57 @@ class About extends React.Component<AboutProps, AboutState> {
                     </div>
                     <div className="team">
                         <h2>Meet the team</h2>
-                        <p>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio atque omnis libero deserunt. Blanditiis ipsa libero perspiciatis non expedita aut doloribus magni reprehenderit repellat, voluptas eaque recusandae ex eveniet aspernatur.
-                        </p>
+                        <div className="card-grid">
+                            <div className="card-row">
+                                <div className="card card--alt">
+                                    <div className="photo-container">
+                                        <img src="/img/SamuelJones.jpg" alt="Photo of Samuel Jones" className="photo" />
+                                    </div>
+                                    <h3 className="card-title">Samuel Jones</h3>
+                                    <p>
+                                        Team leader, front-end designer and developer
+                                    </p>
+                                </div>
+                                <div className="card card--alt">
+                                    <div className="photo-container">
+                                        <img src="/img/KobeJohnson.jpg" alt="Photo of Kobe Johnson" className="photo" />
+                                    </div>
+                                    <h3 className="card-title">Kobe Johnson</h3>
+                                    <p>
+                                        Front-end developer, front-end test engineer
+                                    </p>
+                                </div>
+                                <div className="card card--alt">
+                                    <div className="photo-container">
+                                        <img src="/img/MartinIvanchev.jpg" alt="Photo of Martin Ivanchev" className="photo" />
+                                    </div>
+                                    <h3 className="card-title">Martin Ivanchev</h3>
+                                    <p>
+                                        Back-end developer, database engineer
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="card-row">
+                                <div className="card card--alt">
+                                    <div className="photo-container">
+                                        <img src="/img/AbdulKabbani.jpg" alt="Photo of Abdul Kabbani" className="photo" />
+                                    </div>
+                                    <h3 className="card-title">Abdul Kabbani</h3>
+                                    <p>
+                                        Back-end developer, security engineer
+                                    </p>
+                                </div>
+                                <div className="card card--alt">
+                                    <div className="photo-container">
+                                        <img src="/img/AdamKardorff.jpg" alt="Photo of Adam Kardorff" className="photo" />
+                                    </div>
+                                    <h3 className="card-title">Adam Kardorff</h3>
+                                    <p>
+                                        Back-end developer, security engineer
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="contact-us" id="contact-us">
                         <h2>Contact Us</h2>
